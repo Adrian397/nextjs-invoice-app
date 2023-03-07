410e4fad9bd730b84a7021ec73f49c3774f5fe2c
Adrian Cieplucha <adik.cieplucha@gmail.com>
2023-03-07 23:05:05 +0100

    fix path issue

--- components/InvoiceList/InvoiceListHeader/InvoiceListHeader.utils.ts
+++ components/InvoiceList/InvoiceListHeader/InvoiceListHeader.utils.ts
@@ -1,4 +1,4 @@
-import { Invoice } from "@/components/pagesStylesUtils/index/index.utils";
+import { Invoice } from "@/components/pagesStylesAndUtils/index/index.utils";
 
 export const countFilteredInvoices = (
   activeStatuses: string[],
